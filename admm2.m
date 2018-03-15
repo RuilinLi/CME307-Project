899fbc0c236e787a5c783e9c319311a69d41173d
Philip <paetter@stanford.edu>
2018-03-15 16:57:28 -0700

    finished admm2

--- admm2.m
+++ admm2.m
@@ -1,7 +1,32 @@
-function [X] = admm2(D_sq, hat_D_sq, anchor, N_x_adj, N_a_adj, beta, eps, true_x)
+function [X] = admm2(D_sq, hat_D_sq, anchors, N_x_adj, N_a_adj, beta, eps, true_x)
     max_it = 100;
+    alpha0 = 1;
     
+    n_verts = size(D_sq, 1);
+    d = size(anchors, 1);
     
+    X = zeros(d * n_verts, 1);
+    Y = zeros(d * n_verts, 1);
+    S = zeros(d * n_verts, 1);
+    
+    for i = 1:max_it
+        % Solve system in X
+        f = @(x_) lagrangian(x_, Y, S, D_sq, hat_D_sq, anchors, ...
+            N_x_adj, N_a_adj, beta);
+        grad_f = @(x_) gradient_X_lagrangian(x_, Y, S, D_sq, hat_D_sq, ...
+            anchors, N_x_adj, N_a_adj, beta);
+        [~, X] = bb(f, grad_f, X, alpha0, eps);
+        
+        % Solve system in Y
+        f = @(y_) lagrangian(X, y_, S, D_sq, hat_D_sq, anchors, ...
+            N_x_adj, N_a_adj, beta);
+        grad_f = @(y_) gradient_Y_lagrangian(X, y_, S, D_sq, hat_D_sq, ...
+            anchors, N_x_adj, N_a_adj, beta);
+        [~, Y] = bb(f, grad_f, Y, alpha0, eps);
+        
+        % Update dual variables
+        S = S - beta * (X - Y);
+    end
 end
 
 % Compute the value of the objective function f(X, Y)
